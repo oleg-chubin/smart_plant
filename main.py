85958af67398a8e223c1d9243c35ee5a5b52171f
Pavel Niakhai <3055963@mail.ru>
2022-06-01 11:23:16 +0300

    start make homework 8

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import time
+
 from fake import sensors, switchers
+
+# print(sensors.temperature.get_value())
+# switchers.temperature.turn_on()
+# print(sensors.temperature.get_value())
+
+
+while True:
+    if sensors.temperature.get_value() < 23:
+        switchers.temperature.turn_on()
+    else:
+        switchers.temperature.turn_off()
+    time.sleep(5)
