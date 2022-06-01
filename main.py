c988e65a3a86bfec0bad713b04a4b68efd1f7c60
dimafdrnk <dimawkfdrnk@gmail.comgit config --global user.email dimawkfdrnk@gmail.comgit config --global user.email dimawkfdrnk@gmail.com>
2022-06-01 20:03:02 +0300

    Контроль температуры, версия 1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-from fake import sensors, switchers
+# from fake import sensors, switchers
+import random
+import time
+
+TEMPERATURE = 25
+
+
+def adjustment(current_temperature):
+    while current_temperature != 25:
+        if current_temperature < TEMPERATURE:
+            current_temperature += 1
+            print(current_temperature)
+        else:
+            current_temperature -= 1
+            print(current_temperature)
+        time.sleep(1)
+    return current_temperature
+
+
+while True:
+    current_temperature = random.randint(22, 28)
+    if not current_temperature == TEMPERATURE:
+        current_temperature = adjustment(current_temperature)
+
+    time.sleep(3)
