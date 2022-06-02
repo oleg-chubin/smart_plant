3297f97ffc04b582c576d8bba57351bf3a0b2ea2
dimafdrnk <dimawkfdrnk@gmail.comgit config --global user.email dimawkfdrnk@gmail.comgit config --global user.email dimawkfdrnk@gmail.com>
2022-06-02 17:19:03 +0300

    Обновлена функция конроля тембературы. Создан класс Parametr_control с функцией контроля температуры, но пока без выключателя и сенсора.

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-# from fake import sensors, switchers
 import random
 import time
 
-TEMPERATURE = 25
+class Parameter_control():
+
+    def __init__(self, TEMPERATURE):
+        self.TEMPERATURE = TEMPERATURE
+        # self.switch_status = switch_status
 
 
-def adjustment(current_temperature):
-    while current_temperature != 25:
-        if current_temperature < TEMPERATURE:
-            current_temperature += 1
+    def temperature(self):
+        current_temperature = 0
+        while True:
+            while current_temperature != self.TEMPERATURE:
+                print(current_temperature)
+                if current_temperature < self.TEMPERATURE:
+                    current_temperature += 1
+                else:
+                    current_temperature -= 1
+                time.sleep(1)
             print(current_temperature)
-        else:
-            current_temperature -= 1
-            print(current_temperature)
-        time.sleep(1)
-    return current_temperature
+
+            current_temperature = random.randint(2, 4)
+            time.sleep(5)
+
+test = Parameter_control(3)
+test.temperature()
 
 
-while True:
-    current_temperature = random.randint(22, 28)
-    if not current_temperature == TEMPERATURE:
-        current_temperature = adjustment(current_temperature)
 
-    time.sleep(3)
+
+
+
