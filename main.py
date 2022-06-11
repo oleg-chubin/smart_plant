c554198824326b92ad411b6a5d219df25ab5905b
OpsssDess <OpsssDess@icloud.com>
2022-06-11 19:41:39 +0300

    Нянечка для Кабачка(1задание)

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import time
+
 from fake import sensors, switchers
+
+TEMP = 20
+
+while True:
+    temp_now = sensors.temperature.get_value()
+    if temp_now < TEMP:
+        switchers.temperature.turn_on()
+        print(temp_now)
+    elif temp_now > TEMP:
+        switchers.temperature.turn_off()
+        print(temp_now)
+    time.sleep(2)
+
+
