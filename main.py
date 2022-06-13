f4677b5a14a0c2cbc8ad74c0570e3eb6543d6ec6
OpsssDess <OpsssDess@icloud.com>
2022-06-13 10:35:22 +0300

    Мастерство не пропьёшь(2 часть задания)

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import time
-
 from fake import sensors, switchers
 
-TEMP = 20
+class DeviceOperator():
+    def __init__(self, value, sensor, switcher, parametr):
+        self.value = value
+        self.sensor = sensor
+        self.switcher = switcher
+        self.parametr = parametr
 
-while True:
-    temp_now = sensors.temperature.get_value()
-    if temp_now < TEMP:
-        switchers.temperature.turn_on()
-        print(temp_now)
-    elif temp_now > TEMP:
-        switchers.temperature.turn_off()
-        print(temp_now)
-    time.sleep(2)
+    def check_and_control(self):
+        while True:
+            parametr_now = self.sensor.get_value()
+            if parametr_now < self.value:
+                self.switcher.turn_on()
+                print(self.parametr,parametr_now)
+            elif parametr_now > self.value:
+                self.switcher.turn_off()
+                print(self.parametr, parametr_now)
+            time.sleep(1)
 
+temperature_d_o = DeviceOperator(17, sensors.temperature, switchers.temperature, "temperature")
+temperature_d_o.check_and_control()
 
+moisture_d_o = DeviceOperator(55, sensors.moisture, switchers.moisture, "moisture")
+moisture_d_o.check_and_control()
+
+humidity_d_o = DeviceOperator(55, sensors.humidity, switchers.humidity, "humidity")
+humidity_d_o.check_and_control()
