34ffa2ca6f60b019bab926f729de61f5c56d7e46
OpsssDess <OpsssDess@icloud.com>
2022-06-16 19:31:52 +0300

    Мастерство не пропьёшь(2 часть задания)правки1

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ class DeviceOperator():
         self.parametr = parametr
 
     def check_and_control(self):
-        while True:
             parametr_now = self.sensor.get_value()
             if parametr_now < self.value:
                 self.switcher.turn_on()
@@ -17,13 +16,16 @@ class DeviceOperator():
             elif parametr_now > self.value:
                 self.switcher.turn_off()
                 print(self.parametr, parametr_now)
-            time.sleep(1)
 
-temperature_d_o = DeviceOperator(17, sensors.temperature, switchers.temperature, "temperature")
-temperature_d_o.check_and_control()
-
+temperature_d_o = DeviceOperator(10, sensors.temperature, switchers.temperature, "temperature")
 moisture_d_o = DeviceOperator(55, sensors.moisture, switchers.moisture, "moisture")
-moisture_d_o.check_and_control()
+humidity_d_o = DeviceOperator(57, sensors.humidity, switchers.humidity, "humidity")
 
-humidity_d_o = DeviceOperator(55, sensors.humidity, switchers.humidity, "humidity")
-humidity_d_o.check_and_control()
+switchers.temperature.turn_on()
+switchers.temperature.turn_off()
+
+while True:
+    temperature_d_o.check_and_control()
+    moisture_d_o.check_and_control()
+    humidity_d_o.check_and_control()
+    time.sleep(0.5)
