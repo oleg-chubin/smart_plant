361e377c90372a915c5800eb56f4819b3067a30c
OpsssDess <OpsssDess@icloud.com>
2022-06-21 22:11:33 +0300

    Распорядок для грядок(3 часть задания)фикс периода цикле

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ BIO_CYCLE = [
 ]
 
 class DeviceOperator():
-    def __init__(self, value, sensor, switcher, parametr):
+    def __init__(self, sensor, switcher, parametr, value=0):
         self.value = value
         self.sensor = sensor
         self.switcher = switcher
         self.parametr = parametr
+
+    def set_value(self, value):
+        self.value = value
 
     def check_and_control(self):
             parametr_now = self.sensor.get_value()
@@ -26,29 +29,31 @@ class DeviceOperator():
 
 
 
-def get_need_circle(data):
-    periods = [i['period'] for i in data]
-    all_time = time.time()
-    current_time = all_time % sum(periods)
-    for i, p in enumerate(periods):
-        if p > current_time:
-            break
-        current_time -= p
-    return data[i]
 
-circle_from_period = get_need_circle(BIO_CYCLE)
 
-temperature_d_o = DeviceOperator(circle_from_period['temperature'], sensors.temperature, switchers.temperature, "temperature")
-moisture_d_o = DeviceOperator(circle_from_period['moisture'], sensors.moisture, switchers.moisture, "moisture")
-humidity_d_o = DeviceOperator(circle_from_period['humidity'], sensors.humidity, switchers.humidity, "humidity")
-light_d_o = DeviceOperator(circle_from_period['light'], sensors.light, switchers.light, "light")
+temperature_d_o = DeviceOperator(sensors.temperature, switchers.temperature, "temperature")
+moisture_d_o = DeviceOperator(sensors.moisture, switchers.moisture, "moisture")
+humidity_d_o = DeviceOperator(sensors.humidity, switchers.humidity, "humidity")
+light_d_o = DeviceOperator(sensors.light, switchers.light, "light")
+
+device_operators = [temperature_d_o, moisture_d_o, humidity_d_o, light_d_o]
 
 switchers.temperature.turn_on()
 switchers.temperature.turn_off()
 
 while True:
-    temperature_d_o.check_and_control()
-    moisture_d_o.check_and_control()
-    humidity_d_o.check_and_control()
-    light_d_o.check_and_control()
-    time.sleep(0.5)
+    def get_need_circle(data):
+        periods = [i['period'] for i in data]
+        all_time = time.time()
+        current_time = all_time % sum(periods)
+        for i, p in enumerate(periods):
+            if p > current_time:
+                break
+            current_time -= p
+        return data[i]
+
+    circle_from_period = get_need_circle(BIO_CYCLE)
+    for d_o in device_operators:
+        d_o.set_value(circle_from_period[d_o.parametr])
+        d_o.check_and_control()
+        time.sleep(0.5)
