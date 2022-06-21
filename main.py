fe66481d65174e582301f1f6cb1f4a7b7a24b4dc
Maksim <teramor1986@gmail.com>
2022-06-21 11:18:14 +0300

    forgot constant

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
 from fake import sensors, switchers
+
+TEMP_CONST = 16
 #print(sensors.temperature.get_value())
 #switchers.temperature.turn_on()
 #print(sensors.temperature.get_value())
 while True:
     switchers.temperature.turn_off()
-    if sensors.temperature.get_value() < 16:
+    if sensors.temperature.get_value() < TEMP_CONST:
         switchers.temperature.turn_on()
     time.sleep(1)
 
