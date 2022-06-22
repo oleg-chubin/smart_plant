fffa1c7e3aac1380556da0fb7c42151e3fcf6b8b
Maksim <teramor1986@gmail.com>
2022-06-22 10:56:52 +0300

    added indicators for moisture and humidity.

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ class DeviceOperator():
     def __init__(self, switcher, sensor):
         self.switcher = switcher
         self.sensor = sensor
-
     def set_expected_level(self, value):
         self.value = value
     def check_and_control(self):
         self.switcher.turn_off()
         if self.sensor < self.value:
             self.switcher.turn_on()
-        time.sleep(2)
         print(self.sensor)
 
+indicators =[]
 temperature = DeviceOperator(switchers.temperature, sensors.temperature.get_value())
 temperature.set_expected_level(TEMP_CONST)
-#moisture = DeviceOperator(switchers.moisture, sensors.moisture.get_value(), MOIS_CONST)
-#humidity = DeviceOperator(switchers.humidity, sensors.humidity.get_value(), HUM_CONST)
+indicators.append(temperature)
+moisture = DeviceOperator(switchers.moisture, sensors.moisture.get_value())
+moisture.set_expected_level(MOIS_CONST)
+indicators.append(moisture)
+humidity = DeviceOperator(switchers.humidity, sensors.humidity.get_value())
+humidity.set_expected_level(HUM_CONST)
+indicators.append(humidity)
+
 while True:
-    temperature.check_and_control()
-#moisture.check_and_control()
-#humidity.check_and_control()
+    for indicator in indicators:
+        indicator.check_and_control()
+    time.sleep(2)
