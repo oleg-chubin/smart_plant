3278961f604c12465deaef5c3dd27b0d594f2e01
dimafdrnk <dimawkfdrng@gmail.com>
2022-06-25 17:25:42 +0300

    Цыплят по осени считают. С внешним видом графиков сильно не возился.

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import json
 import time
+
+import openpyxl
+import pandas as pd
+from openpyxl.chart import LineChart, Reference
 
 from fake import sensors, switchers
 
 BIO_CYCLE = [
-    {'period': 36000, 'light': 1000, 'temperature': 23, 'moisture': 80, 'humidity': 34},
-    {'period': 7200, 'light': 500, 'temperature': 20, 'moisture': 85, 'humidity': 34},
-    {'period': 36000, 'light': 0, 'temperature': 18, 'moisture': 70, 'humidity': 45},
-    {'period': 7200, 'light': 500, 'temperature': 20, 'moisture': 85, 'humidity': 34}
+    {'period': 2, 'light': 1000, 'temperature': 16, 'moisture': 55, 'humidity': 48},
+    {'period': 1, 'light': 500, 'temperature': 18, 'moisture': 60, 'humidity': 50},
+    {'period': 1, 'light': 0, 'temperature': 15, 'moisture': 65, 'humidity': 55},
+    {'period': 1, 'light': 500, 'temperature': 14, 'moisture': 60, 'humidity': 60}
 ]
+
+sensors_base = {
+    'light': [sensors.light, switchers.light],
+    'temperature': [sensors.temperature, switchers.temperature],
+    'moisture': [sensors.moisture, switchers.moisture],
+    'humidity': [sensors.humidity, switchers.humidity]
+}
+
+data_for_cycle = [[cycle.get(name) for name in sensors_base] for cycle in BIO_CYCLE]
+
+time_periods = [period.get("period") for period in BIO_CYCLE]
 
 
 class DeviceOperator:
@@ -16,9 +32,8 @@ class DeviceOperator:
         self.switcher = switcher
 
     def check_and_control(self, initial_value):
-        self.initial_value = initial_value
 
-        if self.initial_value > self.sensor.get_value():
+        if initial_value > self.sensor.get_value():
             self.switcher.turn_on()
             return self.sensor.get_value()
         else:
@@ -26,23 +41,58 @@ class DeviceOperator:
             return self.sensor.get_value()
 
 
-base_parametrs = {"light": DeviceOperator(sensors.light, switchers.light),
-                  "temperature": DeviceOperator(sensors.temperature, switchers.temperature),
-                  "moisture": DeviceOperator(sensors.moisture, switchers.moisture),
-                  "humidity": DeviceOperator(sensors.humidity, switchers.humidity)}
+class Chart:
+    shift_chart = 1
 
-while True:
-    def cycle(znach):
-        cycle_change_time = time.time() + next(time_periods)
-        while time.time() < cycle_change_time:
-            for numb, parametr in enumerate(base_parametrs):
-                base_parametrs.get(parametr).check_and_control(znach[numb])
-            time.sleep(1)
+    def __init__(self, column):
+        self.column = column
+
+    def plot_chart(self):
+        for num in range(self.column):
+            chart_type = LineChart()
+            col_and_row = Reference(sheet, min_col=num + 1, min_row=1, max_col=num + 1, max_row=sheet.max_row)
+            chart_type.add_data(col_and_row, titles_from_data=True)
+            sheet.add_chart(chart_type, "L" + str(self.shift_chart))
+            self.shift_chart += 15
 
 
-    time_periods = iter([period.get("period") for period in BIO_CYCLE])
+def writing_reading(log):
+    if log == "open":
+        with open('E:\Python\Учеба\home works\smart_plant1\data.json', 'r') as data:
+            log = json.load(data)
+            return log
+    elif log == log:
+        with open('E:\Python\Учеба\home works\smart_plant1\data.json', 'w') as data:
+            json.dump(log, data, indent=2)
 
-    list(map(cycle, [(cycle.get("light"),
-                      cycle.get("temperature"),
-                      cycle.get("moisture"),
-                      cycle.get("humidity")) for cycle in BIO_CYCLE]))
+
+def control(number_repetitions):
+    for i in range(number_repetitions):
+        for number_cycle, data in enumerate(data_for_cycle):
+            cycle_change_time = time.time() + time_periods[number_cycle]
+            while time.time() < cycle_change_time:
+                for number_parameter, (parameter, operator) in enumerate(sensors_base.items()):
+                    parameter_control = DeviceOperator(sensors_base.get(parameter)[0], sensors_base.get(parameter)[1])
+                    log.setdefault(parameter, [])
+                    data = parameter_control.check_and_control(data_for_cycle[number_cycle][number_parameter])
+                    log.get(parameter).append(data)
+                time.sleep(1)
+
+
+log = writing_reading("open")
+
+control(int(input("Введите количество повторений био-цикла: ")))
+
+writing_reading(log)
+
+log_xl = pd.DataFrame(log)
+log_xl.to_excel("log.xlsx", index=False)
+data = openpyxl.load_workbook("log.xlsx")
+
+sheet = data.active
+
+result = Chart(len(sensors_base))
+result.plot_chart()
+data.save("log.xlsx")
+
+
